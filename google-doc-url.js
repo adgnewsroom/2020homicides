6fbd9a739238060c3b61ccc0f481557a8558ba2a
Arkansas Democrat-Gazette <59581933+adgnewsroom@users.noreply.github.com>
2021-10-05 13:55:04 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,2 +1,2 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/10jiFVCgmIK0MxDUHJrhaMq4Vu9njnHp-JHZesGh9A2U/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/151EaFCc4m0z08eic5L7VkrHUujDjwhJEiHc4zJ0tRqc/edit#gid=0';
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
