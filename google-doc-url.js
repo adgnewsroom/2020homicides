2396801c1efcff27c6c84e17c8231b6272511ea4
Arkansas Democrat-Gazette <59581933+adgnewsroom@users.noreply.github.com>
2021-10-07 13:18:08 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,4 @@
 // paste in your published Google Sheets URL from the browser address bar
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/151EaFCc4m0z08eic5L7VkrHUujDjwhJEiHc4zJ0tRqc/edit#gid=0';
 
-// insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+
